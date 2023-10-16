dbea2f38ee123e814555b0bdbe82c5f9f81ad266
Rafagamedev <rpadiper@gmail.com>
2023-10-16 09:11:28 -0700

    Static site (old)

--- src/components/ButtonList.js
+++
@@ -1,9 +0,0 @@
-import styles from './ButtonList.module.css'
-
-export default function ButtonList (props) {
-  return (
-    <p className={styles.buttonList}>
-      {props.children}
-    </p>
-  )
-}
